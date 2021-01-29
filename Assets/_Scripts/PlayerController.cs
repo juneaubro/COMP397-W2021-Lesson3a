8e21b871850e9d81a5ab53b56fae6111ef57e247
juneau <juneaugawat@hotmail.com>
2021-01-29 15:31:10 -0500

    added CameraController

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 5f;
+    private float movementForce = 5f;
+    [SerializeField]
+    private float jumpForce = 0.35f;
+    private bool isGrounded;
 
     Rigidbody rb;
 
@@ -18,11 +21,34 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") > 0)
-            rb.AddForce(Vector3.right* speed);
+        if (isGrounded)
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0)
+                rb.AddForce(Vector3.right * movementForce);
 
-        if (Input.GetAxisRaw("Horizontal") < 0)
-            rb.AddForce(Vector3.left * speed);
+            if (Input.GetAxisRaw("Horizontal") < 0)
+                rb.AddForce(Vector3.left * movementForce);
 
+            if (Input.GetAxisRaw("Vertical") > 0)
+                rb.AddForce(Vector3.forward * movementForce);
+
+            if (Input.GetAxisRaw("Vertical") < 0)
+                rb.AddForce(Vector3.back * movementForce);
+
+            if (Input.GetAxisRaw("Jump") > 0)
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionStay(Collision c)
+    {
+        if (c.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision c)
+    {
+        if (c.gameObject.CompareTag("Ground"))
+            isGrounded = false;
     }
 }
